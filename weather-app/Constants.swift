62f604e24a5305b369b63b7bb9aa5b7eb3a90ebd
Martin Nordström <martinnord@Martin-Nord.local>
2017-03-09 21:25:34 +0100

    Fixed all the constanst frrom the API

---
+++ weather-app/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  weather-app
+//
+//  Created by Martin Nordström on 2017-03-08.
+//  Copyright © 2017 Martin Nordström. All rights reserved.
+//
+
+import Foundation
+
+let baseUrl = "openweathermap.org/data/2.5/weather?"
+let lat = "lat="
+let long = "&long="
+let appID = "&appid"
+let APIKey = "d8f274c2281319a7eeca5d5f847a53f8"
