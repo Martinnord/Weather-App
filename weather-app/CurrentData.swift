8bd46f6361662c0585c6e3df736a9d58fa1f7064
Martin Nordström <martinnord@Martin-Nord.local>
2017-03-09 21:46:50 +0100

    Added a class and variables that will prevent the app from crashing

---
+++ weather-app/CurrentData.swift
@@ -0,0 +1,54 @@
+//
+//  CurrentData.swift
+//  weather-app
+//
+//  Created by Martin Nordström on 2017-03-09.
+//  Copyright © 2017 Martin Nordström. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class CurrentWeather {
+
+    var _cityName: String!
+    var _date: String!
+    var _weahterType: String!
+    var _currentTemp: Double!
+    
+    // Won't crash
+    var ciryName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long // Will return the full name of the month, the day and the year
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date()) // Transform the date into a string
+        self._date = "Today, \(currentDate)"
+        return _date
+    }
+    
+    
+    var weatherType: String {
+        if _weahterType == nil {
+            _weahterType = ""
+        }
+        return _weahterType
+    }
+    
+    var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+}
